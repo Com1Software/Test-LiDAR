1d2710c28285acd990cb8ddb2d4ed6df75bcb37a
Com1 Software, Inc <information@com1software.com>
2022-10-15 14:27:45 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,63 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"go.bug.st/serial"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	// Retrieve the port list
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
+	}
+
+	// Print the list of detected ports
+	for _, port := range ports {
+		fmt.Printf("Found port: %v\n", port)
+	}
+
+	// Open the first serial port detected at 9600bps N81
+	mode := &serial.Mode{
+		BaudRate: 4800,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+	port, err := serial.Open(ports[0], mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//	line := ""
+	buff := make([]byte, 1)
+	on := true
+	//n := 0
+	for on != false {
+		for {
+			n, err := port.Read(buff)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if n == 0 {
+				fmt.Println("\nEOF")
+				break
+			}
+
+			fmt.Printf("%s", string(buff[:n]))
+
+			// If we receive a newline stop reading
+			if strings.Contains(string(buff[:n]), "\n") {
+				break
+			}
+		}
+	}
+}
